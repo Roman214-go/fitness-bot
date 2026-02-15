cd7de09fad95387598a0bf2a492033b6cbcf5a9c
Roman214-go <gricrom21@gmail.com>
2026-02-16 00:46:25 +0300

    qf

--- src/common/components/Onboarding/Onboarding.tsx
+++ src/common/components/Onboarding/Onboarding.tsx
@@ -77,7 +77,7 @@ export const Onboarding = () => {
   };
 
   return (
-    <div className={styles.wrapper} style={{ padding: isLastSlide ? '0' : '5%' }}>
+    <div className={styles.wrapper} style={{ padding: isLastSlide ? '0' : '5%', paddingTop: '90px' }}>
       {!isLastSlide ? (
         activeIndex === 0 ? (
           <div />
